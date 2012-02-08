8d7770985bd886aa6c3c7c7aea2bf1c658750438
alan pavi i <akapav@gmail.com>
2012-02-08 13:54:01 +0100

    typelib: function support ... progress ...

--- typelib.rkt
+++ typelib.rkt
@@ -5,7 +5,8 @@
 
 (define libgobject (ffi-lib "/usr/lib/libgobject-2.0"))
 
-(define g-type-init (get-ffi-obj "g_type_init" libgobject (_fun -> _void)))
+(define g-type-init
+  (get-ffi-obj "g_type_init" libgobject (_fun -> _void)))
 
 (define ensure-gtypes-initialized
   (let ((initialized #f))
@@ -18,15 +19,27 @@
 
 (define libtl (ffi-lib "/usr/lib/libgirepository-1.0.so"))
 
-(define gir-repostiry-prepend-path
-  (get-ffi-obj "g_irepository_prepend_search_path" libtl
-	       (_fun _string -> _void)))
+;;;
+
+(define _gint _int)
+(define _gboolean _gint)
+
+(define (gbool->bool gbool)
+  (not (zero? gbool)))
 
 (define _typelib-ptr (_cpointer/null 'GITypelib))
 (define _repos-ptr (_cpointer/null 'GIRepository))
+
 (define _base-info-ptr (_cpointer 'GIBaseInfo))
+(define _gobjinfo-ptr (_cpointer/null 'GObjectInfo))
+(define _functioninfo-ptr (_cpointer 'GIFunctionInfo))
+
 (define _gerror-ptr (_cpointer/null 'Gerror))
 (define _gerror-ptr-ptr (_cpointer/null _gerror-ptr))
+
+(define gir-repostiry-prepend-path
+  (get-ffi-obj "g_irepository_prepend_search_path" libtl
+	       (_fun _string -> _void)))
 
 (define _gir-require
   (get-ffi-obj "g_irepository_require" libtl
@@ -42,12 +55,6 @@
 	    (_gir-require #f name #f 0 err))
     (displayln "load error")))
 
-(define gir-require2
-  (get-ffi-obj "g_irepository_require" libtl
-	       (_fun _repos-ptr
-		     _string _string _int  ->
-		     _typelib-ptr)))
-
 (define gir-get-so
   (get-ffi-obj "g_irepository_get_shared_library" libtl
 	       (_fun _repos-ptr
@@ -58,13 +65,13 @@
   (get-ffi-obj "g_irepository_get_n_infos" libtl
 	       (_fun _repos-ptr
 		     _string ->
-		     _int)))
+		     _gint)))
 
 (define gir-get-info
   (get-ffi-obj "g_irepository_get_info" libtl
 	    (_fun _repos-ptr
 		  _string
-		  _int ->
+		  _gint ->
 		  _base-info-ptr)))
 
 (define (gir-info/list ns)
@@ -74,38 +81,15 @@
 
 (define _gir-base-type
   (get-ffi-obj "g_base_info_get_type" libtl
-	       (_fun _base-info-ptr -> _int)))
+	       (_fun _base-info-ptr -> _gint)))
 
-(define (gir-get-type2 base-info)
-  (case (_gir-base-type base-info)
-    ((0) 'invalid)
-    ((1) 'function)
-    ((2) 'callback)
-    ((3) 'struct)
-    ((4) 'boxed)
-    ((5) 'enum)
-    ((6) 'flags)
-    ((7) 'object)
-    ((8) 'interface)
-    ((9) 'constant)
-    ((10) 'invalid)
-    ((11) 'union)
-    ((12) 'value)
-    ((13) 'signal)
-    ((14) 'vfunc)
-    ((15) 'property)
-    ((16) 'field)
-    ((17) 'arg)
-    ((18) 'type)
-    ((19) 'unresolved)))
 
-(begin-for-syntax
-  (define (map-syntax env f stx)
-    (datum->syntax env (map f (syntax->datum stx))))
+(define-for-syntax (map-syntax env f stx)
+  (datum->syntax env (map f (syntax->datum stx))))
 
- (define (predicate-name name)
-   (string->symbol
-    (string-append "gir-type-" (symbol->string name) "?"))))
+(define-for-syntax (base-type-predicate-name name)
+  (string->symbol
+   (string-append "gir-type-" (symbol->string name) "?")))
 
 (define-syntax gir-base-types
   (lambda (stx)
@@ -114,7 +98,7 @@
 	 (with-syntax ((gir-get-type
 			(datum->syntax stx 'gir-get-type))
 		       ((predicate ...)
-			(map-syntax stx predicate-name  #'(name ...))))
+			(map-syntax stx base-type-predicate-name #'(name ...))))
 	   #'(begin
 	       (define (gir-get-type base-info)
 		 (case (_gir-base-type base-info)
@@ -144,9 +128,77 @@
  (arg 17)
  (type 18)
  (unresolved 19))
+
+;;;
+(define (object-info base-info)
+  (cast base-info _base-info-ptr _gobjinfo-ptr))
+
+(define object-get-parent
+  (get-ffi-obj "g_object_info_get_parent" libtl
+	       (_fun _gobjinfo-ptr -> _gobjinfo-ptr)))
+
+(define object-type-name
+  (get-ffi-obj "g_object_info_get_type_name" libtl
+	       (_fun _gobjinfo-ptr -> _string)))
+
+
+(define _object-get-abstract
+  (get-ffi-obj "g_object_info_get_abstract" libtl
+	       (_fun _gobjinfo-ptr -> _gboolean)))
+
+(define (object-abstract? obj-info)
+  (gbool->bool (_object-get-abstract obj-info)))
+
+
+(define object-get-n-mtds
+  (get-ffi-obj "g_object_info_get_n_methods" libtl
+	       (_fun _gobjinfo-ptr -> _gint)))
+
+(define object-get-mtd
+  (get-ffi-obj "g_object_info_get_method" libtl
+	       (_fun _gobjinfo-ptr _gint -> _functioninfo-ptr)))
+
+(define (object-method/list obj-info)
+  (let ((cnt (object-get-n-mtds obj-info)))
+    (for/list ((i (in-range cnt)))
+	      (object-get-mtd obj-info i))))
+
+;;;
+(define function-get-flags
+  (get-ffi-obj "g_function_info_get_flags" libtl
+	       (_fun _functioninfo-ptr -> _int)))
+
+(define-for-syntax (function-flag-predicate-name name)
+  (string->symbol
+   (string-append "function-is-" (symbol->string name) "?")))
+
+(define-syntax function-flags
+  (lambda (stx)
+    (syntax-case stx ()
+      ((_ (name shft) ...)
+       (with-syntax (((predicate ...)
+		      (map-syntax stx
+				  function-flag-predicate-name #'(name ...))))
+	 #'(begin
+	     (define predicate
+	       (lambda (func-info)
+		 (let ((flags (function-get-flags func-info)))
+		   (not (zero?
+			 (bitwise-and flags
+				      (arithmetic-shift 1 shft))))))) ...))))))
+
+(function-flags
+ (method 0)
+ (constructor 1)
+ (getter 2)
+ (setter 3)
+ (wraps-vfunc 4)
+ (throws 5))
+
 ;;;
 (gir-repostiry-prepend-path "/home/aka/devel/girac/1")
-(gir-require2 #f "TestClass" #f 0)
+(gir-require "TestClass")
+
 ;(displayln (gir-get-so #f "tc"))
 
 (define libtc (ffi-lib "/home/aka/devel/girac/1/libtc"))
@@ -154,9 +206,11 @@
 ;(displayln (map gir-get-type (gir-info/list "TestClass")))
 ;;;
 (gir-repostiry-prepend-path "/home/aka/devel/girac/0/code")
-(gir-require2 #f "Lg" #f 0)
+(gir-require "Lg")
 ;(displayln (gir-get-so #f "Lg"))
 
 (define liblg (ffi-lib "/home/aka/devel/girac/0/code/liblg"))
 
-(displayln (filter gir-type-object? (gir-info/list "Lg")))
+(map (compose (lambda (nfo) (map function-is-method? (object-method/list nfo)))
+	      object-info)
+     (filter gir-type-object? (gir-info/list "Lg")))
