aec355200cbe6fede0f2578f73d62c8fb2f5bfea
alan pavičić <akapav@gmail.com>
2012-02-16 15:31:14 +0100

    girepository: loading structures ... in progress ..

--- girepository.rkt
+++ girepository.rkt
@@ -22,30 +22,118 @@
 
 
 ;;; 
-(struct repository
-	(classes funs)
+
+(struct type
+	(tag/interface string pointer?))
+
+(struct argument
+	(type client-allocates? is-optional? is-nullable? ownership))
+
+(struct function
+	(symbol return-type arguments)
 	#:mutable)
 
+(struct gclass
+	(name parent register-function abstract? methods)
+	#:mutable
+	#:transparent)
+
+(struct repository
+	(classes functions)
+	#:mutable
+	#:transparent)
+
+(define (load-class cls type-env)
+  (if (not cls) 'null-class?
+      (let* ((name (gir:class-name cls))
+	     (in-hash (hash-ref type-env name #f)))
+	(or in-hash
+	    (let ((gcls (gclass name
+				(load-class (gir:class-parent cls) type-env)
+				(gir:class-register-function cls)
+				(gir:class-is-abstract? cls)
+				(map (lambda (mtd) (load-function mtd type-env))
+				     (gir:class-methods cls)))))
+	      (hash-set! type-env name gcls)
+	      gcls)))))
+
+(define (load-function fn type-env)
+  (gir:function-symbol fn))
+
+(define (load-gir gir type-env)
+  (gir:repository-require gir)
+  (let ((repos (repository '() '())))
+    (for ((info (gir:repository-info-list gir)))
+	 (let ((info~ (gir:baseinfo-downcast info)))
+	   (cond
+	    ((gir:type-object? info)
+	     (push/list! (load-class info~ type-env)
+			 (repository-classes repos)))
+	    ((gir:type-function? info)
+	     (push/list! (load-function info~ type-env)
+			 (repository-functions repos))))))
+    repos))
+
+;; todo: tmp
+(define (display-repos repos)
+  (for ((cls (repository-classes repos)))
+       (printf "class: ~a~%parent: ~a~%~%"
+	       (gclass-name cls)
+	       (gclass-name (gclass-parent cls)))))
+
+(let ((type-env (make-hash)))
+  (hash-set! type-env "GObject"
+	     (gclass "GObject" #f #f #f '()))
+  (display-repos (load-gir "Gtk" type-env)))
 
 ;;;; test
-(define (do-gir gir)
-  (gir:repository-require gir)
-  (for ((fn (filter gir:type-function? (gir:repository-info-list gir))))
-       (let* ((fn (gir:baseinfo-downcast fn))
-	      (clbl (gir:function->callable fn)))
-	 (displayln
-	  (list
-	   (gir:function-symbol fn)
-	   (gir:foo fn)
-	   (gir:function-type fn)
-	   (gir:function-throwable? fn)
-	   (let ((rt (gir:callable-return-type clbl)))
-	     (list (let ((iface (gir:type-interface rt)))
-		     (if iface (gir:get-type iface) '_))
-		   (gir:type-tag rt)
-		   (gir:type->string rt)
-		   (gir:type-is-pointer? rt)))
+(define (dump-fn fn)
+  (let* ((fn (gir:baseinfo-downcast fn))
+	 (clbl (gir:function->callable fn)))
+    (displayln
+     (list
+      (gir:function-symbol fn)
+      (gir:foo fn)
+      (gir:function-type fn)
+      (gir:function-throwable? fn)
+      (let ((rt (gir:callable-return-type clbl)))
+	(list (let ((iface (gir:type-interface rt)))
+		(if iface (gir:get-type iface) '_))
+	      (gir:type-tag rt)
+	      (gir:type->string rt)
+	      (gir:type-is-pointer? rt)))
+      (map (lambda (arg)
+	     (list
+	      (gir:argument-direction arg)
+	      (if (gir:argument-client-allocates? arg)
+		  'client-allocates
+		  'server-allocates)
+	      (if (gir:argument-is-optional? arg)
+		  'optional
+		  'mandatory)
+	      (if (gir:argument-is-nullable? arg)
+		  'nullable
+		  'not-nullable)
+	      (if (gir:argument-is-return-value? arg)
+		  'return-value
+		  'isnt-return-value)
+	      (gir:argument-ownership-transfer arg)))
 	   (gir:callable-arguments clbl))))))
 
-(do-gir "Gtk")
+(define (dump-obj obj)
+  (let ((obj (gir:baseinfo-downcast obj)))
+    (displayln
+     (list
+      (gir:class-name obj)
+      (gir:class-register-function obj)
+      (gir:class-is-abstract? obj)
+      (gir:class-name (gir:class-parent obj))
+      (map gir:function-symbol (gir:class-methods obj))))))
+
+(define (do-gir gir)
+  (gir:repository-require gir)
+  (for((obj (filter gir:type-object? (gir:repository-info-list gir))))
+      (dump-obj obj)))
+
+;(do-gir "Gtk")
 (collect-garbage)
