eee82fc8e348b408859dcd1559108eb87502cde8
alan pavičić <akapav@gmail.com>
2012-02-27 15:49:35 +0100

    callbacks design ... in progress ...

--- clbck/hello.c
+++ clbck/hello.c
@@ -1,79 +1,9 @@
 #include <gtk/gtk.h>
 #include <stdio.h>
 
-GClosure *closure = NULL;
-
-int(*bar)(int) = NULL;
-
-//
-void pif(int(*f)(int))
-{ printf("f: %d\n", f(1)); }
-
-//
-int cbck(GtkWidget *wdg, GdkEventAny *evt, gpointer p_) {
-  int *p = (int*)p_;
-  printf("callback: %d\n", (*p)++);
-  return *p != 3;
-}
-
-void cbck_notify(gpointer p_, GClosure *closure) { 
-  int *p = (int*)p_;
-  printf("notify: %d\n", *p);
-}
-
-int cnt = 0;
-void foo_closure()
-{ closure = g_cclosure_new(G_CALLBACK(cbck), &cnt, cbck_notify); }
-
-//
-typedef struct {
-  GClosure closure;
-  int foo;
-  int bar;
-} Tito;
-
-static void tito_finalize(gpointer data, GClosure *closure)
-{ puts("ciao tito"); }
-
-Tito *tito_new(gpointer data) {
-  GClosure *closure = g_closure_new_simple(sizeof(Tito), data);
-  Tito *tito = (Tito*)closure;
-
-  g_closure_add_finalize_notifier(closure, data, tito_finalize);
-  return tito;
-}
-
-void marshal(GClosure *closure,
-	     GValue *return_value,
-	     guint n_param_values,
-	     const GValue *param_values,
-	     gpointer invocation_hint,
-	     gpointer marshal_data) {
-  puts("marshaled");
-  puts((char*)marshal_data);
-  static int foo = 1;
-  g_value_set_boolean(return_value, (gboolean)foo--);
-}
-
-void tito_closure() {
-  closure = (GClosure*)tito_new(NULL);
-  g_closure_set_meta_marshal(closure, "foo", marshal);
-}
-
-//
-void half_racket(GClosureMarshal* marshal) {
-  closure = (GClosure*)tito_new(NULL);
-  g_closure_set_marshal(closure, *marshal);
-}
-
-//
-void run(void) {
+void run(GClosure *closure) {
   GtkWidget *window;
   gtk_init(0, NULL);
-  if(!closure) {
-    puts("no closure!");
-    return;
-  }
 
   window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   g_signal_connect_closure(window, "delete_event", closure, 0);
--- clbck/hello.rkt
+++ clbck/hello.rkt
@@ -2,30 +2,54 @@
 
 (require ffi/unsafe)
 
+(define (rc-hash-add! hash key)
+  (hash-set! hash key (add1 (hash-ref hash key 0))))
+
+(define (rc-hash-del! hash key)
+  (let ((cnt (hash-ref hash key 0)))
+    (if (zero? cnt)
+	(hash-remove! hash key)
+	(hash-set! hash key (sub1 cnt)))))
+
+(define *callbacks* (make-hash))
+
 (define libgobject (ffi-lib "/usr/lib/libgobject-2.0"))
 (define libgtk (ffi-lib "/usr/lib/libgtk-3"))
 (define libhello (ffi-lib "./hello"))
 
-(define _closure-ptr (_cpointer 'GClosure))
-(define _gvalue-ptr (_cpointer 'GValue))
-(define _marshall-type (_fun _closure-ptr
-			     _gvalue-ptr
-			     _int
-			     _gvalue-ptr
-			     _pointer
-			     _pointer -> _void))
+;(define _callback-fn (_fun -> void))
 
-(define run (get-ffi-obj "run" libhello (_fun -> _void)))
-(define foo (get-ffi-obj "foo_closure" libhello (_fun -> _void)))
-(define tito (get-ffi-obj "tito_closure" libhello (_fun -> _void)))
-(define rkt (get-ffi-obj "half_racket" libhello (_fun _marshall-type -> _void)))
+(define _callback-notify-fn (_fun #:keep (lambda (c) (rc-hash-add! *callbacks* c))
+				  _pointer
+				  _pointer -> _void))
 
-(define pif (get-ffi-obj "pif" libhello (_fun (_fun _int -> _int) -> _void)))
-(define *closure* (get-ffi-obj "closure" libhello (_cpointer/null 'GClosure)))
+(define _delete-event-fn (_fun _pointer
+			       _pointer
+			       _pointer -> ( ret : _int)
+			       -> (if ret 1 0)))
 
-(pif (lambda (n) (+ 3 n)))
+(define _gclosure-new
+  (get-ffi-obj "g_cclosure_new" libgobject (_fun _pointer
+						 _pointer
+						 _callback-notify-fn -> _pointer)))
 
-(define (marsh a b c d e f) (let loop () (loop)))
-;(rkt marsh)
-(tito)
-(run)
+(define run (get-ffi-obj "run" libhello (_fun _pointer -> _void)))
+
+(define (callback fn type)
+  (rc-hash-add! *callbacks* fn)
+  (_gclosure-new
+   (cast fn type _pointer)
+   #f ;user data is ignored
+   (lambda _
+     (printf "delete callback: ~a~%" _)
+     (rc-hash-del! *callbacks* fn))))
+
+(run (callback
+      (let ((cnt 3))
+	(lambda (a b c)
+	  (begin0
+	      (not (zero? cnt))
+	    (printf "~A~%" cnt)
+	    (collect-garbage)
+	    (set! cnt (sub1 cnt)))))
+      _delete-event-fn))
